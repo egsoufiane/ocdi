b2fc4ff80631c7006330d2449c7c3a09539e37f5
egsoufiane <elg.soufian@gmail.com>
2025-08-02 21:14:12 +0100

    Added script to generate demos json (demos infos)

---
+++ generate_demos.php
@@ -0,0 +1,37 @@
+<?php
+
+$demos = [];
+$base_path = __DIR__; // Adjust if needed
+
+foreach (glob($base_path . '/demo_*') as $folder) {
+    $folder_name = basename($folder);
+    $entry = [
+        'name' => ucfirst(str_replace('_', ' ', $folder_name)),
+        'folder' => $folder_name,
+        'preview' => 'https://your-site.com/' . $folder_name,
+    ];
+
+    if (file_exists("$folder/content.xml")) {
+        $entry['content'] = 'content.xml';
+    }
+    if (file_exists("$folder/widgets.wie")) {
+        $entry['widgets'] = 'widgets.wie';
+    }
+    if (file_exists("$folder/customizer.dat")) {
+        $entry['customizer'] = 'customizer.dat';
+    }
+    if (file_exists("$folder/preview.jpg")) {
+        $entry['preview_image'] = 'preview.jpg';
+    }
+    if (file_exists("$folder/kit.zip")) {
+        $entry['kit'] = true;
+        $entry['kit_file'] = 'kit.zip';
+    }
+
+    $demos[] = $entry;
+}
+
+file_put_contents($base_path . '/demos.json', json_encode($demos, JSON_PRETTY_PRINT));
+echo "demos.json generated successfully.\n";
+
+?>
